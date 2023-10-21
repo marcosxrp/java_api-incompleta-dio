713f5b0c622d2e538f78a4dc3d93e960439cc98b
Marcos Silva <116929557+marcosxrp@users.noreply.github.com>
2023-10-21 18:59:17 -0300

    final

---
+++ src/main/java/me/marcos/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package me.marcos.domain.repository;
+
+import me.marcos.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
